50ba73457f35d311b2308328d473626d78d19f23
jhonny <jhonnyjuncal@gmail.com>
2016-12-28 23:49:26 +0100

    Arreglo del mapa

--- app/src/main/java/com/jhonny/detective/activity/MapaActivity.java
+++ app/src/main/java/com/jhonny/detective/activity/MapaActivity.java
@@ -1,5 +1,6 @@
 package com.jhonny.detective.activity;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -21,6 +22,7 @@ import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -34,143 +36,40 @@ import com.jhonny.detective.Constantes;
 import com.jhonny.detective.util.FileUtil;
 
 
-public class MapaActivity extends Activity {
+public class MapaActivity extends Activity implements OnMapReadyCallback {
 	
-	private GoogleMap mapa = null;
-	
-	
+	private GoogleMap mapa;
+	private Properties propiedades;
+	private Context contexto;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mapa);
-		
-		Properties prefs = new Properties();
-		Context ctx = this;
-		
+		contexto = this;
+
 		try{
-			// objeto mapa con el que trabajar
-			if(mapa == null)
-				mapa = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
-			
-			if(mapa == null)
-				Toast.makeText(getApplicationContext(), "Imposible crear el mapa", Toast.LENGTH_SHORT).show();
-			
-			// tipo de mapa
-			mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-			
-			// lectura del fichero de configuracion
-			prefs = FileUtil.getFicheroAssetConfiguracion(ctx);
-			
-			String tipoCuenta = (String) prefs.get(Constantes.PROP_TIPO_CUENTA);
-			
-			PolylineOptions linea = new PolylineOptions();
-			linea.color(Color.RED);
-			linea.width(1);
-			
-			List<ObjetoPosicion> listaPosiciones = FileUtil.getListaAssetPosiciones(ctx, Integer.parseInt(tipoCuenta));
-			for(int i=0; i<listaPosiciones.size(); i++){
-				ObjetoPosicion obj = listaPosiciones.get(i);
-				if(i == 0)
-					agregarPrimerMarcador(obj.getLatitud(), obj.getLongitud(), obj.getFecha());
-				else
-					agregarMarcador(obj.getLatitud(), obj.getLongitud(), obj.getFecha());
-				linea.add(new LatLng(obj.getLatitud(), obj.getLongitud()));
-			}
-			
-			if(mapa != null){
-				mapa.addPolyline(linea);
-				
-				// listener para el evento onclick de los marcadores
-				mapa.setOnMarkerClickListener(new OnMarkerClickListener() {
-				    public boolean onMarkerClick(Marker marker) {
-				    	marker.showInfoWindow();
-				        return true;
-				    }
-				});
-				
-				mapa.setOnMapLongClickListener(new OnMapLongClickListener() {
-					public void onMapLongClick(LatLng point) {
-						Projection proj = mapa.getProjection();
-						Point coord = proj.toScreenLocation(point);
-				 
-						Toast.makeText(MapaActivity.this,
-							getResources().getString(R.string.label_latitud) + " " + point.latitude + "\n" +
-							getResources().getString(R.string.label_longitud) + " " + point.longitude + "\n" +
-							"X: " + coord.x + "\n" + "Y: " + coord.y, Toast.LENGTH_LONG).show();
-					}
-				});
-				
-				// nivel del zoom
-				if(listaPosiciones != null && listaPosiciones.size() > 0){
-					MapsInitializer.initialize(this);
-					LatLng ubicacion = new LatLng(listaPosiciones.get(0).getLatitud(), listaPosiciones.get(0).getLongitud());
-					
-					CameraPosition camPos = new CameraPosition.Builder()
-					.target(ubicacion)	//Centramos el mapa en Madrid
-					.zoom(19)			//Establecemos el zoom en 19
-					.bearing(45)		//Establecemos la orientación con el noreste arriba
-					.tilt(70)			//Bajamos el punto de vista de la cámara 70 grados
-					.build();
-					
-					CameraUpdate cu = CameraUpdateFactory.newCameraPosition(camPos);
-					mapa.animateCamera(cu);
-				}
-			}
+			MapFragment mapFragment = ((MapFragment) getFragmentManager().findFragmentById(R.id.map));
+			mapFragment.getMapAsync(this);
+
+			propiedades = FileUtil.getFicheroAssetConfiguracion(contexto);
+
 		}catch(Exception ex){
 			ex.printStackTrace();
 		}
 	}
-	
-	
+
 	@Override
 	public void onResume(){
 		super.onResume();
 	}
-	
-	
-	private void agregarMarcador(double latitud, double longitud, Date fecha){
-		try{
-			if(mapa != null){
-				Locale locale = getResources().getConfiguration().locale;
-				mapa.addMarker(new MarkerOptions().position(new LatLng(latitud, longitud))
-						.title(getResources().getString(R.string.label_fecha) + " "
-						+ FileUtil.getFechaFormateada(fecha, locale))
-						.snippet(getResources().getString(R.string.label_hora)
-						+ FileUtil.getHoraFormateada(fecha, locale))
-						.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-						);
-			}
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}
-	}
-	
-	
-	private void agregarPrimerMarcador(double latitud, double longitud, Date fecha){
-		try{
-			if(mapa != null){
-				Locale locale = getResources().getConfiguration().locale;
-				mapa.addMarker(new MarkerOptions().position(new LatLng(latitud, longitud))
-						.title(getResources().getString(R.string.label_fecha) + " "
-						+ FileUtil.getFechaFormateada(fecha, locale))
-						.snippet(getResources().getString(R.string.label_hora)
-						+ FileUtil.getHoraFormateada(fecha, locale))
-						.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-						);
-			}
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}
-	}
-	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu_mapa, menu);
 		return true;
 	}
-	
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -183,5 +82,97 @@ public class MapaActivity extends Activity {
 				return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		this.mapa = googleMap;
+
+		try {
+			cargaPosicionesAlmacenadasEnElMapa();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void cargaPosicionesAlmacenadasEnElMapa() throws IOException {
+		String tipoCuenta = (String) propiedades.get(Constantes.PROP_TIPO_CUENTA);
+
+		PolylineOptions linea = new PolylineOptions();
+		linea.color(Color.RED);
+		linea.width(1);
+
+		List<ObjetoPosicion> listaPosiciones = FileUtil.getListaAssetPosiciones(contexto, Integer.parseInt(tipoCuenta));
+		for(int i=0; i<listaPosiciones.size(); i++){
+			ObjetoPosicion obj = listaPosiciones.get(i);
+			if(i == 0)
+				agregarMarcador(obj.getLatitud(), obj.getLongitud(), obj.getFecha(), true);
+			else
+				agregarMarcador(obj.getLatitud(), obj.getLongitud(), obj.getFecha(), false);
+			linea.add(new LatLng(obj.getLatitud(), obj.getLongitud()));
+		}
+
+		if(mapa != null){
+			mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+			mapa.addPolyline(linea);
+
+			// listener para el evento onclick de los marcadores
+			mapa.setOnMarkerClickListener(new OnMarkerClickListener() {
+				public boolean onMarkerClick(Marker marker) {
+					marker.showInfoWindow();
+					return true;
+				}
+			});
+
+			mapa.setOnMapLongClickListener(new OnMapLongClickListener() {
+				public void onMapLongClick(LatLng point) {
+					Projection proj = mapa.getProjection();
+					Point coord = proj.toScreenLocation(point);
+
+					Toast.makeText(MapaActivity.this,
+							getResources().getString(R.string.label_latitud) + " " + point.latitude + "\n" +
+									getResources().getString(R.string.label_longitud) + " " + point.longitude + "\n" +
+									"X: " + coord.x + "\n" + "Y: " + coord.y, Toast.LENGTH_LONG).show();
+				}
+			});
+
+			// nivel del zoom
+			if(listaPosiciones != null && listaPosiciones.size() > 0){
+				MapsInitializer.initialize(this);
+				LatLng ubicacion = new LatLng(listaPosiciones.get(0).getLatitud(), listaPosiciones.get(0).getLongitud());
+
+				CameraPosition camPos = new CameraPosition.Builder()
+						.target(ubicacion)	//Centramos el mapa en Madrid
+						.zoom(19)			//Establecemos el zoom en 19
+						.bearing(45)		//Establecemos la orientación con el noreste arriba
+						.tilt(70)			//Bajamos el punto de vista de la cámara 70 grados
+						.build();
+
+				CameraUpdate cu = CameraUpdateFactory.newCameraPosition(camPos);
+				mapa.animateCamera(cu);
+			}
+		}
+	}
+
+	private void agregarMarcador(double latitud, double longitud, Date fecha, boolean primero){
+		try{
+			if(mapa != null){
+				Locale locale = getResources().getConfiguration().locale;
+				MarkerOptions marker = new MarkerOptions();
+				marker.position(new LatLng(latitud, longitud));
+				marker.title(getResources().getString(R.string.label_fecha) + " "
+								+ FileUtil.getFechaFormateada(fecha, locale));
+				marker.snippet(getResources().getString(R.string.label_hora) + " "
+								+ FileUtil.getHoraFormateada(fecha, locale));
+				if (primero) {
+					marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+				} else {
+					marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+				}
+				mapa.addMarker(marker);
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
 	}
 }
